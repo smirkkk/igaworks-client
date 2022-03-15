5a6971d02254e6babc8d6bdbd741149f51c6f8ea
Junphill Hwang <junphill.hwang@bountifarm.com>
2022-03-15 14:36:35 +0900

    Feature : route change 이벤트 추가

--- igaworks-client/src/router/index.js
+++ igaworks-client/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
+import axios from '../plugins/axios'
+
 Vue.use(VueRouter)
 
 const routes = [
@@ -24,6 +26,26 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
+
+router.beforeEach(async (to, from, next) => {
+
+  let user_id = window.sessionStorage.getItem('user_id');
+
+  if (user_id == null) {
+    // 유저 아이디 없는 경우 새로 생성
+    user_id = window.guid();
+    window.sessionStorage.setItem('user_id', user_id);
+  }
+
+  axios.post('collect', {
+    user_id: user_id,
+    event_id: window.guid(),
+    event: 'route_change',
+    parameters: JSON.stringify({from: from.path, to: to.path})
+  });
+  
+  return next();
+});
 
 export default router
