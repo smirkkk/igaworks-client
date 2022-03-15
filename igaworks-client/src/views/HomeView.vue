f9cc5acb317e16364954200a84cf4aa7eedd198b
Junphill Hwang <junphill.hwang@bountifarm.com>
2022-03-15 14:48:51 +0900

    Feature : 유저아이디 확인, 갱신 기능 추가

--- igaworks-client/src/views/HomeView.vue
+++ igaworks-client/src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>유저아이디</p>
+    <b>{{userId}}</b>
+    <button @click="generateUserId">갱신</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      userId: undefined
+    }
+  },
+  methods: {
+    generateUserId() {
+      this.userId = window.guid()
+      window.sessionStorage.setItem('user_id', this.userId);
+    }
+  },
+  mounted() {
+    this.userId = window.sessionStorage.getItem('user_id');
   }
 }
 </script>
